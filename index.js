a9aedf05bb0341f358686fad53fc471b8c9c44d5
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-11-26 10:13:53 -0600

    stylized and moved pause button - corrected pause on changing tabs

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
-let running = true;
-
-$("#pause").on('click', function(){
-  console.log("paused");
-  running=!running;
-  if(!running){
-    resume();
-  }
-  else{
-    pause();
-  }
-
-})
-
-$( document ).ready(setup)
-
-// document.addEventListener('visibilitychange', function(){
-//   console.log(running);
-//   running=!running;
-//   if(!running){
-//     resume();
-//   }
-//   else{
-//     pause();
-//   }
-// });
-
 const rippleRes = 768;
 let dripping;
+
+// toggle effect when clicking pause
+$("#pause").on('click', toggle)
+// and when cycling
+document.addEventListener('visibilitychange', toggle)
+
+// ripple effect tobble play/pause
+let running = true;
+function toggle(){
+    console.log("paused");
+    if(!running){
+      resume();
+    }
+    else{
+      pause();
+    }
+    running=!running;
+}
 
 function setup(){
   // set up ripple effect
@@ -47,7 +38,7 @@ function drip(){
     let x = Math.random()*screen.width;
     let y = Math.random()*screen.height;
     let size = 5+(Math.random()*10)
-    let strength = size*.02
+    let strength = size*.002
     $('#intro').ripples("drop", x, y, size, strength)
   }
 
@@ -64,3 +55,6 @@ function resume(){
   // set up timer to run the ripple effect on
   dripping = setInterval(drip, 333)
 }
+
+// Main
+$( document ).ready(setup)
